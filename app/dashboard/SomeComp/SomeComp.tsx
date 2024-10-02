e92944146885ff8ed2a5373f46c7179f14ef6f58
Nikita <perfect1201@ukr.net>
2024-10-02 18:39:19 +0300

    base

---
+++ app/dashboard/SomeComp/SomeComp.tsx
@@ -0,0 +1,4 @@
+import Image from 'next/image'
+export default function SomeComp() {
+  return <Image src="/opengraph-image.png" height={1000} width={1000} alt='img' />;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+import SomeComp from "../SomeComp/SomeComp";
+
+export default function InvoicesPage() {
+  return <>
+  <p>Invoices Page</p>
+  <SomeComp />
+  </>;
+}
